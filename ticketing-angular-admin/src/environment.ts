4d504e7434d11d04ebb01468b8ba330cfb360bf6
Abdun Nafi <nafichasani@gmail.com>
2024-10-12 15:37:52 +0700

    feat: add environment

---
+++ ticketing-angular-admin/src/environment.ts
@@ -0,0 +1,8 @@
+// src/environments/environment.ts
+export const environment = {
+    production: false,
+    apiUrl: 'http://localhost:8000/api/',
+    xenditPublicKey: 'YOUR_XENDIT_PUBLIC_KEY', // Example for Xendit
+    appVersion: '1.0.0',
+  };
+  
