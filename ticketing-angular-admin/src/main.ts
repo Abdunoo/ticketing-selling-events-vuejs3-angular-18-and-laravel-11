6b6e290827dff3b57f8451eb353d89da07968cbd
Abdun Nafi <nafichasani@gmail.com>
2024-10-15 13:43:36 +0700

    fix: env on main.ts

--- ticketing-angular-admin/src/main.ts
+++ ticketing-angular-admin/src/main.ts
@@ -1,6 +1,6 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
-import { environment } from './environment';
+import { environment } from './environment.prod';
 import { AppModule } from './app/app.module';
 
 if (environment.production) {
