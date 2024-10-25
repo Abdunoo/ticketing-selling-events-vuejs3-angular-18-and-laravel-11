97e77914988e7f8f3a9351741068b85834b312f5
Abdun Nafi <nafichasani@gmail.com>
2024-10-25 22:31:26 +0700

    fix: pwa to on vite config

--- ticketing-vue/vite.config.js
+++ ticketing-vue/vite.config.js
@@ -1,9 +1,9 @@
-import { fileURLToPath, URL } from 'node:url';
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
-import viteCompression from 'vite-plugin-compression'; // For asset compression
-import viteImagemin from 'vite-plugin-imagemin'; // For image optimization
-import { VitePWA } from 'vite-plugin-pwa';
+import { fileURLToPath, URL } from "node:url";
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import viteCompression from "vite-plugin-compression"; // For asset compression
+import viteImagemin from "vite-plugin-imagemin"; // For image optimization
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
   plugins: [
@@ -11,46 +11,48 @@ export default defineConfig({
     // Enable gzip or Brotli compression to reduce file size
     viteCompression({
       // Options
-      verbose: true,  // Log the output
+      verbose: true, // Log the output
       disable: false, // Set to true to disable compression
       threshold: 10240, // Only compress files larger than this threshold (in bytes)
-      algorithm: 'gzip', // Compression algorithm (gzip, brotliCompress, deflate, etc.)
-      ext: '.gz' // File extension for the compressed file
+      algorithm: "gzip", // Compression algorithm (gzip, brotliCompress, deflate, etc.)
+      ext: ".gz", // File extension for the compressed file
     }),
     // Optimize images during build
     viteImagemin({
       gifsicle: { optimizationLevel: 7 },
       optipng: { optimizationLevel: 7 },
-      mozjpeg: { quality: 75 }, // Adjust quality to balance size and quality
-      pngquant: { quality: [0.65, 0.8], speed: 4 },
-      svgo: {
-        plugins: [
-          { name: 'removeViewBox', active: false },
-          { name: 'removeEmptyAttrs', active: true },
-        ],
-      },
+      mozjpeg: { quality: 75 },
+      pngquant: { quality: [0.7, 0.8] },
+      webp: { quality: 75 },
     }),
     VitePWA({
-      registerType: 'autoUpdate',
-      workbox: {
-        runtimeCaching: [
+      registerType: "autoUpdate",
+      // includeAssets: ["favicon.icon", "robots.txt"],
+      manifest: {
+        name: "Ticketku",
+        short_name: "Ticketku",
+        description: "A web application for managing tickets efficiently.",
+        theme_color: "#0d7cf2",
+        background_color: "#ffffff",
+        display: "standalone",
+        start_url: "/ticketing",
+        id: "/ticketing",
+        icons: [
           {
-            urlPattern: /^https:\/\/your-api\.com\/.*$/, // Adjust this to your API URL
-            handler: 'NetworkFirst', // Cache strategy
-            options: {
-              cacheName: 'api-cache',
-              expiration: {
-                maxEntries: 50,
-                maxAgeSeconds: 60 * 60 * 24 * 30, // 30 days
-              },
-            },
+            src: "/ticketing/assets/image/logo_192x192.webp",
+            sizes: "192x192",
+            type: "image/webp",
           },
           {
-            urlPattern: /^https:\/\/fonts\.googleapis\.com\/.*/, // Example for caching Google Fonts
-            handler: 'CacheFirst',
-            options: {
-              cacheName: 'google-fonts-stylesheets',
-            },
+            src: "/ticketing/assets/image/logo_512x512.webp",
+            sizes: "512x512",
+            type: "image/webp",
+          },
+          {
+            src: "/ticketing/assets/image/logo_512x512_maskable.webp",
+            sizes: "512x512",
+            type: "image/webp",
+            purpose: "any maskable",
           },
         ],
       },
@@ -58,20 +60,20 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
   },
-  base: '/ticketing', // Ensure the base path starts and ends with '/'
+  base: "/ticketing", // Ensure the base path starts and ends with '/'
   build: {
-    target: 'es2015', // Set the target to improve compatibility across browsers
-    minify: 'esbuild', // Ensure ESBuild minification for faster builds
+    target: "es2015", // Set the target to improve compatibility across browsers
+    minify: "esbuild", // Ensure ESBuild minification for faster builds
     cssCodeSplit: true, // Enable CSS code splitting to reduce bundle size
     rollupOptions: {
       output: {
         manualChunks(id) {
           // Create separate chunks for dependencies, reducing bundle size
-          if (id.includes('node_modules')) {
-            return 'vendor';
+          if (id.includes("node_modules")) {
+            return "vendor";
           }
         },
       },
