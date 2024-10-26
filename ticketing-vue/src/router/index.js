31c355d549a575eb9395b539d8aa46b8f5a17d1d
Abdun Nafi <nafichasani@gmail.com>
2024-10-26 19:58:04 +0700

    fix: iredirect to login

--- ticketing-vue/src/router/index.js
+++ ticketing-vue/src/router/index.js
@@ -2,111 +2,112 @@ import { useAuthStore } from "@/stores/auth";
 import { createRouter, createWebHistory } from "vue-router";
 const routes = [
   // Route
-  { 
-    path: "/", 
-    name: "Dashboard", 
-    component: () => import('@/views/Dashboard.vue')
+  {
+    path: "/",
+    name: "Dashboard",
+    component: () => import("@/views/Dashboard.vue"),
   },
-  { 
-    path: "/all-for-you", 
-    name: "Events", 
-    component: () => import('@/views/EventCategory.vue')
+  {
+    path: "/all-for-you",
+    name: "Events",
+    component: () => import("@/views/EventCategory.vue"),
   },
-  { 
-    path: "/:event_name", 
-    name: "EventDetail", 
-    component: () => import('@/views/EventDetail.vue')
+  {
+    path: "/:event_name",
+    name: "EventDetail",
+    component: () => import("@/views/EventDetail.vue"),
   },
-  { 
-    path: "/checkout", 
-    name: "Checkout", 
-    component: () => import('@/views/Checkout.vue'),
-    meta: { requiresAuth: true }
+  {
+    path: "/checkout",
+    name: "Checkout",
+    component: () => import("@/views/Checkout.vue"),
+    meta: { requiresAuth: true },
   },
   // Route auth
-  { 
-    path: "/sign_up", 
-    name: "SignUp", 
-    component: () => import('@/views/auth/Register.vue')
+  {
+    path: "/sign_up",
+    name: "SignUp",
+    component: () => import("@/views/auth/Register.vue"),
   },
-  { 
-    path: "/otp", 
-    name: "Otp", 
-    component: () => import('@/views/auth/Otp.vue')
+  {
+    path: "/otp",
+    name: "Otp",
+    component: () => import("@/views/auth/Otp.vue"),
   },
-  { 
-    path: "/login", 
-    name: "Login", 
-    component: () => import('@/views/auth/Login.vue')
+  {
+    path: "/login",
+    name: "Login",
+    component: () => import("@/views/auth/Login.vue"),
   },
   // Route Form
-  { 
-    path: "/events_create", 
-    name: "CreateEvent", 
-    component: () => import('@/views/form/CreateEvent.vue'),
-    meta: { requiresAuth: true }
+  {
+    path: "/events_create",
+    name: "CreateEvent",
+    component: () => import("@/views/form/CreateEvent.vue"),
+    meta: { requiresAuth: true },
   },
   // Other
-  { 
-    path: "/my_tickets", 
-    name: "Mytickets", 
-    component: () => import('@/views/Mytickets.vue'),
-    meta: { requiresAuth: true }
+  {
+    path: "/my_tickets",
+    name: "Mytickets",
+    component: () => import("@/views/Mytickets.vue"),
+    meta: { requiresAuth: true },
   },
-  { 
-    path: "/my_tickets/:id", 
-    name: "MyticketsDetail", 
-    component: () => import('@/views/DetailOrder.vue'),
-    meta: { requiresAuth: true }
+  {
+    path: "/my_tickets/:id",
+    name: "MyticketsDetail",
+    component: () => import("@/views/DetailOrder.vue"),
+    meta: { requiresAuth: true },
   },
-  { 
-    path: "/account", 
-    name: "Account", 
-    component: () => import('@/views/Account.vue'),
-    meta: { requiresAuth: true }
+  {
+    path: "/account",
+    name: "Account",
+    component: () => import("@/views/Account.vue"),
+    meta: { requiresAuth: true },
   },
-  { 
-    path: "/my_events", 
-    name: "MyEvents", 
-    component: () => import('@/views/MyEvent.vue'),
-    meta: { requiresAuth: true }
+  {
+    path: "/my_events",
+    name: "MyEvents",
+    component: () => import("@/views/MyEvent.vue"),
+    meta: { requiresAuth: true },
   },
-  { 
-    path: "/pricing", 
-    name: "Pricing", 
-    component: () => import('@/views/Pricing.vue'),
+  {
+    path: "/pricing",
+    name: "Pricing",
+    component: () => import("@/views/Pricing.vue"),
   },
   // Other again
   {
-    path: '/about',
-    name: 'About',
-    component: () => import('@/views/footer/About.vue')
+    path: "/about",
+    name: "About",
+    component: () => import("@/views/footer/About.vue"),
   },
   {
-    path: '/terms',
-    name: 'Terms',
-    component: () => import('@/views/footer/Terms.vue')
+    path: "/terms",
+    name: "Terms",
+    component: () => import("@/views/footer/Terms.vue"),
   },
   {
-    path: '/privacy',
-    name: 'Privacy',
-    component: () => import('@/views/footer/Privacy.vue')
+    path: "/privacy",
+    name: "Privacy",
+    component: () => import("@/views/footer/Privacy.vue"),
   },
   {
-    path: '/contact',
-    name: 'Contact',
-    component: () => import('@/views/footer/Contact.vue')
+    path: "/contact",
+    name: "Contact",
+    component: () => import("@/views/footer/Contact.vue"),
   },
   {
-    path: '/admin',
+    path: "/admin",
     beforeEnter() {
-      window.location.href = 'https://sandbox2.panemu.com/ticketing/admin/index.html';
-    }
-  }
+      window.location.href =
+        "https://sandbox2.panemu.com/ticketing/admin/index.html";
+    },
+  },
 ];
 
 const router = createRouter({
-  history: createWebHistory("/ticketing"), 
+  history: createWebHistory("/ticketing"),
   routes,
 });
 
@@ -114,8 +115,13 @@ router.beforeEach((to, from, next) => {
   const authStore = useAuthStore();
 
   if (to.meta.requiresAuth && !authStore.isLogin) {
-    authStore.setRedirectPath(to.fullPath); // Save the intended path
-    next({ name: 'Login' });
+    let info = localStorage.getItem("isLogin");
+    info = JSON.parse(info);
+    if (!info) {
+      authStore.setRedirectPath(to.fullPath); // Save the intended path
+      next({ name: "Login" });
+    }
+    next();
   } else {
     next();
   }
